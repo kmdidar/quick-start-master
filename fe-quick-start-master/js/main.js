808248ef64fb32c57ab969e195367fe9939e2f11
kmdidar <kmdidar@gmail.com>
2017-07-08 04:19:55 +0600

    quickstart

--- fe-quick-start-master/js/main.js
+++ fe-quick-start-master/js/main.js
@@ -1,13 +1,36 @@
 $(document).ready(function($){
 	"use strict";
 
-	// WOW Js Active
-	new WOW().init();
+	jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        // WOW Js Active
+		new WOW().init();
 
 	// ---- Active
-    $(".owl").owlCarousel();
+    	$(".owl").owlCarousel();
     // Owl Next Privew Change
     //$( ".owl-prev").html('<i class="fa screenshort-arow fa-chevron-left"></i>');
     //$( ".owl-next").html('<i class="fa screenshort-arow fa-chevron-right"></i>');
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+	
 
 })(jQuery);
